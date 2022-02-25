a9af64ec4faa7a43326a685f80e7f9cf45e211ed
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-02-25 07:19:02 -0300

    creating transaction card and using flatList

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+
 import { HighlightCard } from "../../components/HighlightCard";
-import { TransactionCard } from "../../components/TransactionCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
 import {
   Container, 
   Header,
@@ -13,10 +14,52 @@ import {
   Icon,
   HighlightCards,
   Transactions,
-  Title
+  Title,
+  TransactionList
  } from "./styles";
 
-export function Dashboard() {
+
+ export interface DataListProps extends TransactionCardProps {
+  id:string;
+}
+ 
+ export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title:"Desenvolvimento de site",
+      amount:"R$ 12.000,00",
+      category:{
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date:"13/04/2020"
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title:"Hamburgueria Pizzy",
+      amount:"R$ 59,00",
+      category:{
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date:"10/04/2021"
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title:"Aluguel do apartamento",
+      amount:"R$ 1.200,00",
+      category:{
+        name: 'Casa',
+        icon: 'shopping-bag'
+      },
+      date:"10/04/2021"
+    }
+  ]
+
   return (
     <Container>
       <Header>
@@ -58,9 +101,11 @@ export function Dashboard() {
       <Transactions>
         <Title>Listagem</Title>
 
-        <TransactionCard />
-        <TransactionCard />
-        
+       <TransactionList
+        data={data}
+        keyExtractor={item => item.}
+        renderItem={({ item }) => <TransactionCard data={item}/>}
+        />   
       </Transactions>
     </Container>
   )
